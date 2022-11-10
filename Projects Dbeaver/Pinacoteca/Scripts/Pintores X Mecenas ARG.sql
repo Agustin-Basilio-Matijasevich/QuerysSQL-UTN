e23865ff5efa91b420c26b4ece7eea5cf3cfb43a
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-11-09 21:34:39 -0300

    Eliminacion todos los projects

--- Projects Dbeaver/Pinacoteca/Scripts/Pintores X Mecenas ARG.sql
+++
@@ -1,6 +0,0 @@
-#Listar nombres de pintores argentinos o mecenas que apoyaron a pintores argentinos
-
-select null as Pintor, Apellido_Nombre as Nombre_Pintor, p.Ciudad, null as Mecena, Nombre as Nombre_Mecena, m.Pais, m.Ciudad
-from pintor p, mecena m, mecena_pintor mp
-where p.Pais = 'Argentina' and m.ID = mp.ID_Mecena and p.ID = mp.ID_Pintor
-order by Apellido_Nombre asc, Nombre asc;
--- Projects Dbeaver/sample/Scripts/Alumnos X Carrera.sql
+++
@@ -1,3 +0,0 @@
-select Nombre, NombreCarrera  from sample.alumno_carrera ac, sample.carrera c, sample.alumno a  
-where CodCarreraFKPK = CodCarreraPK and DNIFKPK = DNI
-order by Nombre;
